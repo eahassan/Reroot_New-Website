eb26b839944cc51dbeeaec93817cd49de97e1fbe
Evan H <eahassan@oakland.edu>
2019-09-20 12:09:45 -0400

    More styling & added carousel for site

--- js/Site.js
+++ js/Site.js
@@ -1,5 +1,18 @@
 /* Site.js File */
 
+$(function () {
+    // Click on play button, so carousel can keep sliding between images!
+    $('#playBtn').click(function () {
+        $('#myCarousel').carousel('cycle');
+    });
+
+    // Click on pause button to stop carousel from moving!
+    $("#pauseBtn").click(function () {
+        $("#myCarousel").carousel("pause");
+    });
+    
+    });
+
 /* Tooltips */   
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
