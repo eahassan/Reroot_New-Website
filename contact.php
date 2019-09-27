e3ab707938ce467d16d3de43c0a6939bc7caef4c
Evan H <eahassan@oakland.edu>
2019-09-27 13:30:34 -0400

    Created contact form

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contact Us - Reroot</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Evan H.">
+    <meta name="description" content="Reroot Pontiac's Green Infrastructure Site">
+    <meta name="keywords" content="Reroot Pontiac, Green Infrastructure, Green Technology">
+    <!-- Bootstra 4 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- FontAwesome Icons and CSS-->   
+    <script src="https://kit.fontawesome.com/8c89b82d38.js" crossorigin="anonymous"></script>
+    <!-- FontAwesome Social Media Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Google Fonts - Karma -->    
+    <link href="https://fonts.googleapis.com/css?family=Karma&display=swap" rel="stylesheet">
+    <!-- styles CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/icons/green-leave.ico">
+</head>    
+<body>
+<?php
+
+
+
+
+?>
+
+<h1>Reroot</h1>   
+    <hr>
+ <form action="#" method="post">
+     
+     
+ </form>      
+        
+                
+</body>    
+</html>
