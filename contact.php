fcac9bb4fe4b341ed1b51495d40afbb017ac5575
Evan H <eahassan@oakland.edu>
2019-10-03 19:56:42 -0400

    Created draft of contact form and added icons to images directory

--- contact.php
+++ contact.php
@@ -23,17 +23,72 @@
 <body>
 <?php
 
-
+echo " ";
 
 
 ?>
 
-<h1>Reroot</h1>   
+<!-- https://mdbootstrap.com/docs/jquery/forms/contact/#antispam -->
+<section>
+    <h1 class="font-weight-bold text-center">Contact Us</h1>
     <hr>
- <form action="#" method="post">
-     
-     
- </form>      
+    <p class="text-center">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+        a matter of hours to help you.</p>
+
+    <div class="row">
+
+        <div class="col-md-9">
+            <form action="#" method="POST">
+           
+            <div class="form-group">  <!-- <i class="fas fa-user"></i> -->
+            <label for="name" class="font-weight-bolder">Name:</label>    
+            <input type="text" id="name" name="name" class="form-control" placeholder="Enter your Name"><br>
+            </div>                  
+            
+            <div class="form-group">    <!-- <i class="fas fa-envelope"></i> -->
+            <label for="email" class="font-weight-bolder">E-Mail:</label>         
+            <input type="text" id="email" name="email" class="form-control" placeholder="Enter your E-Mail"><br>
+            </div>
+                
+            <div class="form-group">   <!-- <i class="fas fa-leaf"></i> -->           
+            <label for="subject" class="font-weight-bolder">Subject:</label>       
+            <input type="text" id="subject" name="subject" class="form-control" placeholder="Enter your Subject line"><br>
+            </div>  
+                
+            <div class="form-group">   <!-- <i class="fas fa-comment-alt"></i> -->               
+            <label for="message" class="font-weight-bolder">Message:</label>        
+            <textarea type="text" id="message" name="message" rows="2" class="form-control" placeholder="Enter your Message here..."></textarea>
+            </div>      
+
+                    
+            
+
+            </form> <br><br>
+
+            <div class="text-center">
+                <a class="btn btn-primary marg-botm">Send</a>
+            </div>
+        </div>
+ 
+        
+        <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p class="lead">76 Henderson Street, Pontiac, MI 48341</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p class="lead">+ (248)-703-0288</p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p class="lead">info@rerootpontiac.org</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</section> 
         
                 
 </body>    
